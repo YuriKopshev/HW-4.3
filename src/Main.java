83ae5bb768377ab2d6abb8c136d13b5a4f5b61f9
Yuri Kopshev <yura.kopshev@gmail.com>
2020-07-17 11:17:19 +0200

    add java files

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        AnnuityPayment annuityPay = new AnnuityPayment();
+        int payment1 = (int) annuityPay.calculate(1_000_000,9.99,12);
+        int payment2 = (int) annuityPay.calculate(1_000_000,9.99,24);
+        int payment3 = (int) annuityPay.calculate(1_000_000,9.99,36);
+        System.out.println(payment1);
+        System.out.println(payment2);
+        System.out.println(payment3);
+    }
+}
