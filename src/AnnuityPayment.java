9671fd7290d962e0a68c0871114e2256c81f0b43
Yuri Kopshev <yura.kopshev@gmail.com>
2020-07-18 07:42:45 +0200

    code fixed

--- src/AnnuityPayment.java
+++ src/AnnuityPayment.java
@@ -1,9 +1,9 @@
 public class AnnuityPayment {
-    public double calculate(int sum,double rate,int period){
-        double monthRate = (rate/12)/100;
-        double x=1+monthRate;
-        double y = Math.pow(x,period);
-        double annuityRate= monthRate*y/(y-1);
-        return (sum*annuityRate);
+    public double calculate(int sum, double rate, int period) {
+        double monthRate = (rate / 12) / 100;
+        double x = 1 + monthRate;
+        double y = Math.pow(x, period);
+        double annuityRate = monthRate * y / (y - 1);
+        return (sum * annuityRate);
     }
 }
